a01cccc64e511be27065de65159ba007db9eaa54
Vladimir <74771567+8bit-com@users.noreply.github.com>
2026-06-11 03:46:59 +0700

    Keep server route outside Wintun

--- src/main/java/com/example/demo/windows/WintunNetworkConfigurator.java
+++ src/main/java/com/example/demo/windows/WintunNetworkConfigurator.java
@@ -9,11 +9,15 @@ public class WintunNetworkConfigurator {
     private static final String CLIENT_IP = "10.8.0.2";
     private static final String MASK = "255.255.255.0";
     private static final String TEST_ROUTE = "10.8.0.1";
+    private static final String SERVER_IP = "80.240.23.72";
+    private static final String HOME_GATEWAY = "192.168.1.1";
 
     private final WindowsCommand windowsCommand = new WindowsCommand();
 
     public void configure() {
         deleteTestRoute();
+        deleteServerRoute();
+        addServerRoute();
         setAddress();
         int interfaceIndex = findInterfaceIndex();
         addTestRoute(interfaceIndex);
@@ -21,6 +25,7 @@ public class WintunNetworkConfigurator {
 
     public void cleanup() {
         deleteTestRoute();
+        deleteServerRoute();
     }
 
     private void deleteTestRoute() {
@@ -28,6 +33,27 @@ public class WintunNetworkConfigurator {
                 "route",
                 "delete",
                 TEST_ROUTE
+        ));
+    }
+
+    private void deleteServerRoute() {
+        windowsCommand.runIgnoreError(List.of(
+                "route",
+                "delete",
+                SERVER_IP
+        ));
+    }
+
+    private void addServerRoute() {
+        windowsCommand.run(List.of(
+                "route",
+                "add",
+                SERVER_IP,
+                "mask",
+                "255.255.255.255",
+                HOME_GATEWAY,
+                "metric",
+                "1"
         ));
     }
 
