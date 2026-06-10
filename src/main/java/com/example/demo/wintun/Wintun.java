fa5bd3b1e4bf62c45a8452ac68eec461cfb7f4c6
Vladimir <74771567+8bit-com@users.noreply.github.com>
2026-06-11 02:35:19 +0700

    Remove jna-platform types from Wintun interface

--- src/main/java/com/example/demo/wintun/Wintun.java
+++ src/main/java/com/example/demo/wintun/Wintun.java
@@ -4,23 +4,21 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.WString;
-import com.sun.jna.platform.win32.WinDef.DWORD;
-import com.sun.jna.platform.win32.WinNT.HANDLE;
 import com.sun.jna.ptr.IntByReference;
 
 public interface Wintun extends Library {
 
     Wintun INSTANCE = Native.load("wintun", Wintun.class);
 
-    HANDLE WintunCreateAdapter(WString name, WString tunnelType, Pointer requestedGuid);
+    Pointer WintunCreateAdapter(WString name, WString tunnelType, Pointer requestedGuid);
 
-    void WintunCloseAdapter(HANDLE adapter);
+    void WintunCloseAdapter(Pointer adapter);
 
-    HANDLE WintunStartSession(HANDLE adapter, DWORD capacity);
+    Pointer WintunStartSession(Pointer adapter, int capacity);
 
-    void WintunEndSession(HANDLE session);
+    void WintunEndSession(Pointer session);
 
-    Pointer WintunReceivePacket(HANDLE session, IntByReference packetSize);
+    Pointer WintunReceivePacket(Pointer session, IntByReference packetSize);
 
-    void WintunReleaseReceivePacket(HANDLE session, Pointer packet);
+    void WintunReleaseReceivePacket(Pointer session, Pointer packet);
 }
