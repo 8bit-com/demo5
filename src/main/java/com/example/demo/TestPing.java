5ff76f3d1dde90cc1e309abd8d47aafdd9afcfd9
Vladimir <v.dementev@bftcom.com>
2026-06-11 21:20:31 +0700

    fix

--- src/main/java/com/example/demo/TestPing.java
+++ src/main/java/com/example/demo/TestPing.java
@@ -6,47 +6,64 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class TestPing {
+    private static final String SERVER_IP = "80.240.23.72";
+    private static final int PORT = 51888;
+
     public void start() {
         try {
-            String serverIp = "80.240.23.72"; // сюда IP твоего сервера
-            int port = 51888;
-
             DatagramSocket socket = new DatagramSocket();
 
-            byte[] data = "PING".getBytes();
+            for (int i = 0; i < 1000; i++) {
+                send(socket, i);
 
-            DatagramPacket packet = new DatagramPacket(
-                    data,
-                    data.length,
-                    InetAddress.getByName(serverIp),
-                    port
-            );
-
-            socket.send(packet);
-
-            System.out.println("PING sent");
-
-            byte[] buffer = new byte[1024];
-
-            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-
-            socket.receive(response);
-
-            String text = new String(
-                    response.getData(),
-                    0,
-                    response.getLength()
-            );
-
-            System.out.println("Response: " + text);
+                receive(socket);
+            }
 
             socket.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    private void send(DatagramSocket socket, int i) throws IOException {
+        socket.send(getPacket(i));
+        System.out.println("PING sent " + i);
+    }
+
+    private void receive(DatagramSocket socket) throws IOException {
+        byte[] buffer = new byte[1024];
+
+        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+
+        socket.receive(response);
+
+        printResponse(response);
+    }
+
+    private void printResponse(DatagramPacket response) {
+        String text = new String(
+                response.getData(),
+                response.getOffset(),
+                response.getLength(),
+                StandardCharsets.UTF_8
+        );
+        System.out.println("Response: " + text);
+    }
+
+    private DatagramPacket getPacket(int i) throws UnknownHostException {
+        String message = "PING " + i;
+        byte[] data = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(
+                data,
+                data.length,
+                InetAddress.getByName(SERVER_IP),
+                PORT
+        );
+        return packet;
     }
 }
